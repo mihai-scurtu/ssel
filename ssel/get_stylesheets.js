1daeef075f736fe09587291839d696517f2980f3
Mihai Scurtu <mscurtu90@gmail.com>
2014-06-27 13:01:12 +0300

    Also retrieve css content. More strict file filtering.

--- ssel/get_stylesheets.js
+++ ssel/get_stylesheets.js
@@ -3,7 +3,22 @@ var stylesheets = [];
 $('link[rel=stylesheet]').each(function(i) {
 	var url = $(this).attr('href');
 	var filename = url.split('/').slice(-1)[0];
-	stylesheets.push({'url': url, 'filename': filename});
+	var content;
+
+	// ignore cdn sheets
+	if(!filename.match(/^\/\//)) {
+		$.ajax({
+			url: url,
+			type: 'get',
+			async: false,
+			success: function(r) {
+				content = r;
+			} 
+		});
+		if(content) {
+			stylesheets.push({'url': url, 'filename': filename, 'content': content});
+		}
+	}
 });
 
 stylesheets;
