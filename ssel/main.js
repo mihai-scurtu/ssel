b2dc2931f46dd0dbb7c41832ba0040a30f152fb9
Mihai Scurtu <mscurtu90@gmail.com>
2014-06-27 13:01:54 +0300

    Change default functionality to inline css display.

--- ssel/main.js
+++ ssel/main.js
@@ -1,11 +1,36 @@
 var stylesheets = [];
 
+$(function () {
+	$('.sheet-title').on('click', function(e) {
+		e.preventDefault();
+		$(this).parent().find('.sheet-content').slideToggle();
+	});
+});
+
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 	chrome.tabs.executeScript(tabs[0].id, {file: 'get_stylesheets.js'}, function(results) {
 		stylesheets = stylesheets.concat(results[0]);
 
 		for(var i = 0; i < stylesheets.length; i++) {
-			$('#stylesheets').append('<li>' + stylesheets[i].filename + '</li>');
+			var li = $(document.createElement('li'));
+
+			var title = $(document.createElement('a'))
+				.attr({
+					href: '#',
+				})
+				.addClass('sheet-title')
+				.text(stylesheets[i].filename);
+
+			title.on('click', function(e) {
+				e.preventDefault();
+				$(this).parent().find('.sheet-content').slideToggle();
+			});
+
+			var content = $(document.createElement('div'))
+				.addClass('sheet-content')
+				.html(stylesheets[i].content.replace(/\n+/g, '<br>'))
+				.hide();
+			$('#stylesheets').append(li.append(title).append(content));
 		}
 	});
 });
